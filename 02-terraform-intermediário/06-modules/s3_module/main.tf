104c2d53284963a1555d0d471f5420c999b15351
Karlla Nascimento <karlla.nascimento@creditas.com>
2023-11-26 11:03:02 -0300

    chore: usando modulos do terraform

---
+++ 02-terraform-intermediário/06-modules/s3_module/main.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "this" {
+
+  bucket = var.name
+
+  acl = var.acl
+
+
+  policy = var.policy
+
+
+  dynamic "website" {
+
+    for_each = length(keys(var.website)) == 0 ? [] : [var.website]
+
+    content {
+
+      index_document           = lookup(website.value, "index_document", null)
+      error_document           = lookup(website.value, "error_document", null)
+      redirect_all_requests_to = lookup(website.value, "redirect_all_requests_to", null)
+      routing_rules            = lookup(website.value, "routing_rules", null)
+    }
+  }
+
+  tags = var.tags
+}
---
+++ 02-terraform-intermediário/06-modules/s3_module/variables.tf
@@ -0,0 +1,57 @@
+variable "aws_region" {
+  type        = string
+  description = ""
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = ""
+  default     = "estudoterraform"
+}
+
+variable "name" {
+  type        = string
+  description = "Bucket anem"
+}
+
+variable "acl" {
+
+  type        = string
+  description = ""
+  default     = "private"
+}
+
+variable "policy" {
+  type        = string
+  description = ""
+  default     = null
+}
+
+variable "tags" {
+  type        = map(string)
+  description = ""
+  default     = {}
+}
+
+variable "key_prefix" {
+  type    = string
+  default = ""
+}
+
+variable "files" {
+  type    = string
+  default = ""
+}
+
+variable "website" {
+  description = "Map containing website configuration."
+  type        = map(string)
+  default     = {}
+}
+
+variable "versioning" {
+  description = "Map containing versioning configuration."
+  type        = map(string)
+  default     = {}
+}
